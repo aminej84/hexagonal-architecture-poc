7a0176501d57f2e562a397af8d02936a3944b204
aminej84 <38663816+aminej84@users.noreply.github.com>
2021-02-17 10:22:04 +0100

    Initial commit

---
+++ infrastructure/src/main/java/fr/hexagonal/infrastructure/config/SpringDataJpaAdapterConfiguration.java
@@ -0,0 +1,16 @@
+package fr.hexagonal.infrastructure.config;
+
+import fr.hexagonal.domain.spi.ProductPersistencePort;
+import fr.hexagonal.infrastructure.adapter.ProductSpringJpaAdapter;
+import fr.hexagonal.infrastructure.repository.ProductRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDataJpaAdapterConfiguration {
+
+    @Bean
+    public ProductPersistencePort getProductPersistencPort(ProductRepository productRepository) {
+        return new ProductSpringJpaAdapter(productRepository);
+    }
+}
---
+++ infrastructure/src/main/java/fr/hexagonal/infrastructure/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package fr.hexagonal.infrastructure.repository;
+import fr.hexagonal.infrastructure.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
+}
